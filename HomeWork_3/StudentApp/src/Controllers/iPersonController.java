3952550c40a15d95f973a8c16ffe76a7ccfb31a2
Daniil <danilovski.men-soligorsk@mail.ru>
2023-06-18 12:14:37 +0300

    rename 4 hm

--- HomeWork_3/StudentApp/src/Controllers/iPersonController.java
+++
@@ -1,7 +0,0 @@
-package Controllers;
-
-import StudentDomen.Person;
-
-public interface iPersonController<T extends Person> {
-    void create(String firstName, String secondName, int age);    
-}
--- HomeWork_3/StudentApp/src/StudentDomen/Emploee.java
+++
@@ -1,9 +0,0 @@
-package StudentDomen;
-
-public class Emploee extends Person{
-    private String special;
-    public Emploee(String firstName, String secondName, int age, String special) {
-        super(firstName, secondName, age);
-        this.special = special;
-    }
-}
